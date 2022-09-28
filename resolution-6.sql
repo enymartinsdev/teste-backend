7429c6bbb1999d65bf7b10598256f3f78ae20958
Eny Martins <enyalvesmartins@gmail.com>
2022-09-28 17:28:40 -0300

    feat: add SQL

--- resolution-6.sql
+++ resolution-6.sql
@@ -4,3 +4,9 @@
 -- (id, data, produto_id, qtde). 
 -- Monte uma query em SQL que retorne as 
 -- vendas mensais de cada produto (mes, produto, qtde, valor total).
+
+SELECT data, produto.nome, sum(qtde) as "quantity", sum(valor) as "total" from vendas
+LEFT JOIN produto
+on produto.id = vendas.produto_id
+WHERE month(data) = 8
+GROUP BY 2
