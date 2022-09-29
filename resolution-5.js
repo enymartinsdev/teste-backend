799c25371b3ba25a0ed95a34f0a1fb1f73661325
Eny Martins <enyalvesmartins@gmail.com>
2022-09-29 16:10:09 -0300

    feat: add resolution-5

--- resolution-5.js
+++ resolution-5.js
@@ -1,3 +1,32 @@
-// Manipulação de arquivos
-// Escreva uma função em NodeJS que receba a URL de um arquivo CSV 
-// e retorne um array de objetos utilizando o cabeçalho do arquivo como propriedades.
+// // Manipulação de arquivos
+// // Escreva uma função em NodeJS que receba a URL de um arquivo CSV
+// // e retorne um array de objetos utilizando o cabeçalho do arquivo como propriedades.
+const fs = require("fs");
+const readline = require("readline");
+const stream = fs.createReadStream("./file.csv");
+const rl = readline.createInterface({ input: stream });
+
+function jsonDataArray(array) {
+  let headers = array[0].split(",");
+  let jsonData = [];
+  for (let i = 1, length = array.length; i < length; i++) {
+    let row = array[i].split(",");
+    let data = {};
+    for (let x = 0; x < row.length; x++) {
+      data[headers[x]] = row[x];
+    }
+    jsonData.push(data);
+  }
+
+  return jsonData;
+}
+
+let finalData = [];
+
+rl.on("line", (row) => {
+  finalData.push(row);
+});
+
+rl.on("close", () => {
+  console.log(jsonDataArray(finalData));
+});
