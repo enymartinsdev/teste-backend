3da076d93397491906ece41780de5bded51c8b08
Eny Martins <enyalvesmartins@gmail.com>
2022-09-29 20:34:59 -0300

    feat: add new solution to ts

--- resolution-4.js
+++ resolution-4.js
@@ -3,53 +3,82 @@
 //Trate os possíveis erros ao instanciar. 
 //Em seguida, adicione os métodos getUnix() e getDatetime()
 //que retornam a data utilizada na construção em formato UNIX ou 'YYYY-MM-DD HH:mm:ss' respectivamente.
-// Format using reusable function
-function padTo2Digits(num) {
-    return num.toString().padStart(2, '0'); //every time will be with 2 digits, even when 01:00:00
-}
-// format as "hh:mm:ss YYYY-MM-DD "
-function formatDate(date) {
-    return ([
-        padTo2Digits(date.getHours()),
-        padTo2Digits(date.getMinutes()),
-        padTo2Digits(date.getSeconds()),
-    ].join(':')
-        + ' ' +
-        [
-            padTo2Digits(date.getDate()),
-            padTo2Digits(date.getMonth() + 1),
-            date.getFullYear(),
-        ].join('/'));
-}
-var GetTheDate = /** @class */ (function () {
-    function GetTheDate() {
-        this.date = formatDate(new Date());
+var GetFormatDate = /** @class */ (function () {
+    function GetFormatDate(dateString) {
+        this.date = new Date(this.formater(dateString));
     }
-    GetTheDate.prototype.getUnix = function () {
-        this.date = formatDate(new Date());
-        var _a = this.date.split(' '), getHours = _a[0], getData = _a[1];
-        var _b = getData.split('/'), day = _b[0], month = _b[1], year = _b[2];
-        var _c = getHours.split(':'), hours = _c[0], minutes = _c[1], seconds = _c[2];
-        var date = new Date(+year, +month - 1, +day, +hours, +minutes, +seconds);
-        var timestampInMs = date.getTime();
-        console.log("min", timestampInMs);
-        var timestampInSeconds = Math.floor(date.getTime() / 1000); //unix mode
-        console.log("unix", timestampInSeconds);
+    GetFormatDate.prototype.getUnix = function () {
+        console.log("getUnix", this.date.getTime());
     };
-    GetTheDate.prototype.getDateTime = function () {
-        var formatedDate = formatDate(new Date());
-        this.date = formatedDate;
-        var _a = this.date.split(' '), getHours = _a[0], getData = _a[1];
-        var dia = getData.split("/")[0];
-        var mes = getData.split("/")[1];
-        var ano = getData.split("/")[2];
-        console.log("day", dia, "mes", mes, "ANO", ano);
-        var formatedYearFirst = ano + '-' + mes + '-' + dia;
-        console.log("finalResult", formatedYearFirst + ' ' + getHours);
-        return formatedYearFirst;
+    GetFormatDate.prototype.getDateTime = function () {
+        console.log("getDateTime", this.date.toISOString()
+            .slice(0, 19)
+            .replace('T', ' ')
+            .replace(/\-/g, '/'));
     };
-    return GetTheDate;
+    GetFormatDate.prototype.formater = function (dateString) {
+        var dateTime = dateString.split(' ')[0];
+        var date = dateString.split(' ')[1];
+        var year = date.split('/')[2];
+        var month = date.split('/')[1];
+        var day = date.split('/')[0];
+        var finalDate = "".concat(year, "/").concat(month, "/").concat(day, " ").concat(dateTime);
+        return finalDate;
+    };
+    return GetFormatDate;
 }());
-var getDates = new GetTheDate();
-getDates.getUnix();
-getDates.getDateTime();
+var setDate = new GetFormatDate('17:20:00 29/09/2022');
+setDate.getUnix();
+setDate.getDateTime();
+//Format using reusable function
+// function padTo2Digits(num: number) { 
+//     return num.toString().padStart(2, '0'); //every time will be with 2 digits, even when 01:00:00
+// }
+// format as "hh:mm:ss YYYY-MM-DD "
+// function formatDate(date: Date) {
+//     return (
+//         [
+//             padTo2Digits(date.getHours()),
+//             padTo2Digits(date.getMinutes()),
+//             padTo2Digits(date.getSeconds()),
+//         ].join(':')
+//         + ' ' +
+//         [
+//             padTo2Digits(date.getDate()),
+//             padTo2Digits(date.getMonth() + 1),
+//             date.getFullYear(),
+//         ].join('/')
+//     )
+// }
+// class GetTheDate {
+//     date: string
+//     constructor() {
+//         this.date = formatDate(new Date())
+//     }
+//     getUnix(): void {
+//         this.date = formatDate(new Date())
+//         let [getHours, getData] = this.date.split(' ')
+//         const [day, month, year] = getData.split('/');
+//         const [hours, minutes, seconds] = getHours.split(':');
+//         const date = new Date(+year, +month - 1, +day, +hours, +minutes, +seconds);
+//         const timestampInMs = date.getTime();
+//         console.log("min", timestampInMs)
+//         const timestampInSeconds = Math.floor(date.getTime() / 1000); //unix mode
+//         console.log("unix", timestampInSeconds)
+//     }
+//     getDateTime(): string {
+//         const formatedDate = formatDate(new Date())
+//         this.date = formatedDate
+//         let [getHours, getData] = this.date.split(' ')
+//         let dia = getData.split("/")[0];
+//         let mes = getData.split("/")[1];
+//         let ano = getData.split("/")[2];
+//         console.log("day", dia, "mes", mes, "ANO", ano)
+//         const formatedYearFirst = ano + '-' + mes + '-' + dia
+//         console.log("finalResult", formatedYearFirst + ' ' + getHours)
+//         return formatedYearFirst
+//     }
+// }
+// let getDates = new GetTheDate()
+// getDates.getUnix()
+// getDates.getDateTime()
--- resolution-4.ts
+++ resolution-4.ts
@@ -4,72 +4,109 @@
 //Em seguida, adicione os métodos getUnix() e getDatetime()
 //que retornam a data utilizada na construção em formato UNIX ou 'YYYY-MM-DD HH:mm:ss' respectivamente.
 
-// Format using reusable function
-function padTo2Digits(num: number) {
-    return num.toString().padStart(2, '0'); //every time will be with 2 digits, even when 01:00:00
+
+
+class GetFormatDate {
+
+    date: Date;
+    constructor(dateString: string) {
+        this.date = new Date(this.formater(dateString))
+    }
+
+    getUnix() {
+        console.log("getUnix", this.date.getTime())
+    }
+
+    getDateTime() {
+        console.log("getDateTime", this.date.toISOString()
+            .slice(0, 19)
+            .replace('T', ' ')
+            .replace(/\-/g, '/'))
+    }
+
+    formater(dateString: string) {
+        const dateTime = dateString.split(' ')[0]
+        const date = dateString.split(' ')[1]
+        const year = date.split('/')[2]
+        const month = date.split('/')[1]
+        const day = date.split('/')[0]
+
+        const finalDate = `${year}/${month}/${day} ${dateTime}`
+
+        return finalDate
+    }
 }
+
+const setDate = new GetFormatDate('17:20:00 29/09/2022')
+setDate.getUnix()
+setDate.getDateTime()
+
+//Format using reusable function
+// function padTo2Digits(num: number) { 
+//     return num.toString().padStart(2, '0'); //every time will be with 2 digits, even when 01:00:00
+// }
 
 // format as "hh:mm:ss YYYY-MM-DD "
-function formatDate(date: Date) {
-    return (
-        [
-            padTo2Digits(date.getHours()),
-            padTo2Digits(date.getMinutes()),
-            padTo2Digits(date.getSeconds()),
-        ].join(':')
-        + ' ' +
-        [
-            padTo2Digits(date.getDate()),
-            padTo2Digits(date.getMonth() + 1),
-            date.getFullYear(),
-        ].join('/')
-    )
-}
+// function formatDate(date: Date) {
+//     return (
+//         [
+//             padTo2Digits(date.getHours()),
+//             padTo2Digits(date.getMinutes()),
+//             padTo2Digits(date.getSeconds()),
+//         ].join(':')
+//         + ' ' +
+//         [
+//             padTo2Digits(date.getDate()),
+//             padTo2Digits(date.getMonth() + 1),
+//             date.getFullYear(),
+//         ].join('/')
+//     )
+// }
 
 
 
-class GetTheDate {
-    date: string
+// class GetTheDate {
+//     date: string
 
-    constructor() {
-        this.date = formatDate(new Date())
-    }
+//     constructor() {
+//         this.date = formatDate(new Date())
+//     }
 
-    getUnix(): void {
-        this.date = formatDate(new Date())
+//     getUnix(): void {
+//         this.date = formatDate(new Date())
 
-        let [getHours, getData] = this.date.split(' ')
+//         const [getHours, getData] = this.date.split(' ')
 
-        const [day, month, year] = getData.split('/');
-        const [hours, minutes, seconds] = getHours.split(':');
+//         const [day, month, year] = getData.split('/');
+//         const [hours, minutes, seconds] = getHours.split(':');
 
-        const date = new Date(+year, +month - 1, +day, +hours, +minutes, +seconds);
+//         const date = new Date(+year, +month - 1, +day, +hours, +minutes, +seconds);
 
-        const timestampInMs = date.getTime();
-        console.log("min", timestampInMs)
+//         const timestampInMs = date.getTime();
+//         console.log("min", timestampInMs)
 
-        const timestampInSeconds = Math.floor(date.getTime() / 1000); //unix mode
-        console.log("unix", timestampInSeconds)
-    }
+//         const timestampInSeconds = Math.floor(date.getTime() / 1000); //unix mode
+//         console.log("unix", timestampInSeconds)
+//     }
 
-    getDateTime(): string {
-        const formatedDate = formatDate(new Date())
-        this.date = formatedDate
+//     getDateTime(): string {
+//         const formatedDate = formatDate(new Date())
+//         this.date = formatedDate
 
-        let [getHours, getData] = this.date.split(' ')
+//         const [getHours, getData] = this.date.split(' ')
 
-        let dia = getData.split("/")[0];
-        let mes = getData.split("/")[1];
-        let ano = getData.split("/")[2];
-        console.log("day", dia, "mes", mes, "ANO", ano)
+//         const dia = getData.split("/")[0];
+//         const mes = getData.split("/")[1];
+//         const ano = getData.split("/")[2];
+//         console.log("day", dia, "mes", mes, "ANO", ano)
 
-        const formatedYearFirst = ano + '-' + mes + '-' + dia
-        console.log("finalResult", formatedYearFirst + ' ' + getHours)
-        return formatedYearFirst
-    }
+//         const formatedYearFirst = ano + '-' + mes + '-' + dia
+//         console.log("finalResult", formatedYearFirst + ' ' + getHours)
+//         return formatedYearFirst
+//     }
 
-}
+// }
 
-let getDates = new GetTheDate()
-getDates.getUnix()
-getDates.getDateTime()
+// const getDates = new GetTheDate()
+// getDates.getUnix()
+// getDates.getDateTime()
